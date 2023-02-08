262b94d904d6d39a063bec360e64722097c23d25
Pankaj Saini <121940275+pankajnkt9@users.noreply.github.com>
2023-02-08 13:10:14 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,21 @@
+function currentTime() {
+    let date = new Date()
+    let hour = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+    let session = "AM";
+    if (hour === 0) { hour = 12; }
+    if (hour > 12) {
+        hour = hour - 12;
+        session = "PM";
+    }
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sc : sec;
+    let time = hour + ":" + min + ":" + sec + "" + session;
+    document.getElementById("clock").innerText = time;
+    let t = setTimeout(function () {
+        currentTime();
+    }, 1000);
+}
+currentTime();
